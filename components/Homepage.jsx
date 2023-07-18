08b87613c3e15cfa3590b6a09bf0157f4462aec2
Bryan W <bhoff1980@gmail.com>
2023-07-18 01:09:49 -0600

    hero section completed with app buttons

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
   return (
     <>
     <Carousel>
-      <Carousel.Item>
+      <Carousel.Item interval={null}>
         <img
           className="d-block w-100"
           src="Demo-landing-slide.png"
@@ -38,14 +38,15 @@ const Homepage = () => {
     </Carousel>
     <div>
       <h1 className='text-[#CB1F13] text-center pt-[96px] pb-[32px] font-alegreya'>A Fountain Explodes... A Scroll Discovered... An Adventure Begins </h1>
-      <p className='text-center'>Set in Spanish Inquisition Spain, Unscrolled takes you ona journey of self discovery. 
+      <p className='text-center text-lg'>Set in Spanish Inquisition Spain, Unscrolled takes you ona journey of self discovery. 
         
         <br/> You'll travel the world, play action games, and make important decisions. </p>
         <div className='ml-[55%] mt-[-65px]'>
         <img src='critical-thinking.png' />
         </div>
-        <div>
-          
+        <div className='flex gap-10 justify-center mb-10'>
+          <img src='apple.png' className='gap-3'/>
+          <img src='google-play.png'/>
         </div>
     </div>
  
