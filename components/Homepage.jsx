73df4dc3a07c485902c914bd5418be574158f1d3
Bryan W <bhoff1980@gmail.com>
2023-07-17 23:19:49 -0600

    built carousel with demo image and filler images

--- components/Homepage.jsx
+++ components/Homepage.jsx
@@ -1,11 +1,40 @@
+"use client"
 import React from 'react'
+import { Carousel } from 'react-bootstrap';
 
 const Homepage = () => {
   return (
-    <div>
-      <h1 className='text-center text-7xl mt-28'>Landing Page (Home Route)</h1>
-    </div>
-  )
+    <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="Demo-landing-slide.png"
+          alt="First slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="coming-soon.jpg"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="coming-soon.jpg"
+          alt="Third slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="coming-soon.jpg"
+          alt="Fourth slide"
+        />
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
 export default Homepage
